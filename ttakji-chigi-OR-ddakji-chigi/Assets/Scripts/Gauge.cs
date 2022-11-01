6c5f4b3b790cfd36784041da43bdcbeaa68167d9
tjdnjsrmsdidgkrdlfma <tjdnjsrmsdidgkrdlfma@gmail.com>
2022-11-01 20:42:58 +0900

    딱지치기 기본적인 기능은 전부 구현했습니다.
    딱지가 뒤집힐 때 이펙트를 넣도록 하겠습니다.

--- ttakji-chigi-OR-ddakji-chigi/Assets/Scripts/Gauge.cs
+++ ttakji-chigi-OR-ddakji-chigi/Assets/Scripts/Gauge.cs
@@ -4,34 +4,44 @@ using UnityEngine;
 
 public class Gauge : MonoBehaviour
 {
+    public float gauge;
+    public bool stop_gauge_moving;
+
     const float x_coord = 0.5f;
 
-    float gauge;
     float y_position;
     bool longer;
 
+
     void Awake()
     {
-        gauge = 5;
+        gauge = 1;
+        stop_gauge_moving = false;
+
         y_position = 0;
         longer = false;
     }
 
     void Update()
     {
+        if (stop_gauge_moving == false)
+            ChangeGuage();
+    }
+
+    void ChangeGuage()
+    {
         if (gauge <= 0 && longer == false)
             longer = true;
-        else if (gauge >= 5 && longer == true)
+        else if (gauge >= 1 && longer == true)
             longer = false;
 
         if (longer == false)
-            gauge -= 0.1f;
+            gauge -= 0.02f;
         else if (longer == true)
-            gauge += 0.1f;
+            gauge += 0.02f;
 
-        y_position = gauge * 0.5f - 2.5f;
+        y_position = gauge * 2.2f - 2.2f;
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, y_position, gameObject.transform.position.z);
         gameObject.transform.localScale = new Vector3(x_coord, gauge, 1);
-
     }
 }
---
+++ ttakji-chigi-OR-ddakji-chigi/Assets/Scripts/TtakjiManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TtakjiManager : MonoBehaviour
+{
+    GameObject Ttakji0;
+    GameObject Ttakji1;
+    GameObject Gauge;
+
+    void Awake()
+    {
+        Ttakji0 = GameObject.Find("Ttakji0");
+        Ttakji1 = GameObject.Find("Ttakji1");
+        Gauge = GameObject.Find("Gauge/Gauge");
+    }
+
+    public void OnTtakjiMoveButtonClicked()
+    {
+        Gauge.GetComponent<Gauge>().stop_gauge_moving = true;
+
+        StartCoroutine("MoveTtakji");
+    }
+
+    IEnumerator MoveTtakji()
+    {
+        float ttakji0_y = Ttakji0.transform.position.y;
+        float temp;
+
+        while (Ttakji1.transform.position.y > ttakji0_y)
+        {
+            Ttakji1.transform.position = new Vector3(Ttakji1.transform.position.x, Ttakji1.transform.position.y - 0.2f, Ttakji1.transform.position.z);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        for (int i = 0; i < 50; i++)
+        {
+            temp = Mathf.Lerp(Ttakji0.transform.position.y, 0, 0.05f);
+            Ttakji0.transform.position = new Vector3(Ttakji0.transform.position.x, temp, Ttakji0.transform.position.z);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        while (Ttakji0.transform.position.y > -3.3f)
+        {
+            Ttakji0.transform.position = new Vector3(Ttakji0.transform.position.x, Ttakji0.transform.position.y - 0.2f, Ttakji0.transform.position.z);
+            yield return null;
+        }
+
+        FlipTtakji();
+    }
+
+    void FlipTtakji()
+    {
+        float gauge = Gauge.GetComponent<Gauge>().gauge;
+
+        Ttakji0.GetComponent<SpriteRenderer>().sortingOrder = 3;
+
+        if (gauge > 0.9f)
+        {
+            //Ttakji0.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Textures/Ttakji1");// as Sprite; <- ÀÌ°Ç ¾È µÊ
+            //Ttakji0.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Textures/µüÁö µÚÁýÈù ½ºÇÁ¶óÀÌÆ®");// as Sprite;
+        }
+    }
+
+
+}
