576acdb14d89a3f7a153257a3516f3b205ce7ac7
tjdnjsrmsdidgkrdlfma <tjdnjsrmsdidgkrdlfma@gmail.com>
2022-11-02 10:05:11 +0900

    딱지치기 이펙트 추가했습니다.

--- ttakji-chigi-OR-ddakji-chigi/Assets/howan/Ttakji/Scripts/TtakjiManager.cs
+++ ttakji-chigi-OR-ddakji-chigi/Assets/howan/Ttakji/Scripts/TtakjiManager.cs
@@ -4,32 +4,34 @@ using UnityEngine;
 
 public class TtakjiManager : MonoBehaviour
 {
-    GameObject Ttakji0;
-    GameObject Ttakji1;
-    GameObject Gauge;
+    GameObject ttakji0;
+    GameObject ttakji1;
+    GameObject gauge;
+    GameObject blink;
 
     void Awake()
     {
-        Ttakji0 = GameObject.Find("Ttakji0");
-        Ttakji1 = GameObject.Find("Ttakji1");
-        Gauge = GameObject.Find("Gauge/Gauge");
+        ttakji0 = GameObject.Find("Ttakji0");
+        ttakji1 = GameObject.Find("Ttakji1");
+        gauge = GameObject.Find("Gauge/Gauge");
+        blink = GameObject.Find("Canvas").transform.Find("Blink").gameObject;
     }
 
     public void OnTtakjiMoveButtonClicked()
     {
-        Gauge.GetComponent<Gauge>().stop_gauge_moving = true;
+        gauge.GetComponent<Gauge>().stop_gauge_moving = true;
 
         StartCoroutine("MoveTtakji");
     }
 
     IEnumerator MoveTtakji()
     {
-        float ttakji0_y = Ttakji0.transform.position.y;
+        float ttakji0_y = ttakji0.transform.position.y;
         float temp;
 
-        while (Ttakji1.transform.position.y > ttakji0_y)
+        while (ttakji1.transform.position.y > ttakji0_y)
         {
-            Ttakji1.transform.position = new Vector3(Ttakji1.transform.position.x, Ttakji1.transform.position.y - 0.2f, Ttakji1.transform.position.z);
+            ttakji1.transform.position = new Vector3(ttakji1.transform.position.x, ttakji1.transform.position.y - 0.2f, ttakji1.transform.position.z);
             yield return null;
         }
 
@@ -37,34 +39,82 @@ public class TtakjiManager : MonoBehaviour
 
         for (int i = 0; i < 50; i++)
         {
-            temp = Mathf.Lerp(Ttakji0.transform.position.y, 0, 0.05f);
-            Ttakji0.transform.position = new Vector3(Ttakji0.transform.position.x, temp, Ttakji0.transform.position.z);
+            temp = Mathf.Lerp(ttakji0.transform.position.y, 0, 0.05f);
+            ttakji0.transform.position = new Vector3(ttakji0.transform.position.x, temp, ttakji0.transform.position.z);
+            yield return null;
+        }
+
+        for (int i = 0; i < 15; i++)
+        {
+            ttakji0.transform.Rotate(new Vector3(0, 0, i * -7));
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
             yield return null;
         }
 
         yield return new WaitForSeconds(0.5f);
 
-        while (Ttakji0.transform.position.y > -3.3f)
+        blink.SetActive(true);
+
+        yield return new WaitForSeconds(0.5f);
+
+        blink.SetActive(false);
+
+        yield return new WaitForSeconds(0.1f);
+
+        FlipTtakji();
+
+        while (ttakji0.transform.position.y > -3.3f)
         {
-            Ttakji0.transform.position = new Vector3(Ttakji0.transform.position.x, Ttakji0.transform.position.y - 0.2f, Ttakji0.transform.position.z);
+            ttakji0.transform.position = new Vector3(ttakji0.transform.position.x, ttakji0.transform.position.y - 0.2f, ttakji0.transform.position.z);
             yield return null;
         }
 
-        FlipTtakji();
+        yield return new WaitForSeconds(0.5f);
+
+        StartCoroutine(ShowEffect());
     }
 
     void FlipTtakji()
     {
-        float gauge = Gauge.GetComponent<Gauge>().gauge;
+        float gauge = this.gauge.GetComponent<Gauge>().gauge;
 
-        Ttakji0.GetComponent<SpriteRenderer>().sortingOrder = 3;
+        ttakji0.GetComponent<SpriteRenderer>().sortingOrder = 3;
 
-        if (gauge > 0.9f)
+        if (gauge > 0.85f)
         {
-            Ttakji0.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Ttakji/Ttakji1");// as Sprite; <- ÀÌ°Ç ¾È µÊ
+            ttakji0.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Ttakji/TtakjiBack0");// as Sprite; <- ÀÌ°Ç ¾È µÊ
             //Ttakji0.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Textures/µüÁö µÚÁýÈù ½ºÇÁ¶óÀÌÆ®");// as Sprite;
         }
     }
 
+    IEnumerator ShowEffect()
+    {
+        float gauge = this.gauge.GetComponent<Gauge>().gauge;
 
+        GameObject effect = GameObject.Find("Ttakji0/Effect");
+
+        if (gauge > 0.85f)
+        {
+            effect.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Ttakji/Success");
+        }
+        else
+        {
+            effect.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Ttakji/Fail");
+        }
+
+        for (int i = 0; i < 60; i++)
+        {
+            effect.transform.Rotate(new Vector3(0, 0, i * 5));
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+        }
+
+        effect.GetComponent<SpriteRenderer>().sprite = null;
+    }
 }
